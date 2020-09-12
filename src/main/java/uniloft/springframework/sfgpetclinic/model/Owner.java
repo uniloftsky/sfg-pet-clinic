9498c1ac9385933a2a2ebb3dfda093e4b08140f4
toOXaUL <thetooxa@gmail.com>
2020-09-13 00:17:40 +0300

    Adding POJOs. Closes #2.

---
+++ src/main/java/uniloft/springframework/sfgpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package uniloft.springframework.sfgpetclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/uniloft/springframework/sfgpetclinic/model/Vet.java
@@ -0,0 +1,7 @@
+package uniloft.springframework.sfgpetclinic.model;
+
+public class Vet extends Person {
+
+
+
+}
