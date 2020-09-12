a143b7dd792e896180a88fe909972058ea6e32bc
toOXaUL <thetooxa@gmail.com>
2020-09-13 02:34:20 +0300

    Implements interfaces  for services. Closes #5

---
+++ pet-clinic-data/src/main/java/uniloft/springframework/sfgpetclinic/services/PetService.java
@@ -0,0 +1,15 @@
+package uniloft.springframework.sfgpetclinic.services;
+
+import uniloft.springframework.sfgpetclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+    Pet findById(Long id);
+
+    Pet save(Pet pet);
+
+    Set<Pet> findAll();
+
+}
