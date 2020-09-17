95e097402123da7497c5d9be348eaee1813a7c55
uniloftsky <uniloftsky@gmail.com>
2020-09-18 00:16:56 +0300

    Added PetTypeService. Closes #29

---
+++ pet-clinic-data/src/main/java/uniloft/springframework/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package uniloft.springframework.sfgpetclinic.services;
+
+import uniloft.springframework.sfgpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
