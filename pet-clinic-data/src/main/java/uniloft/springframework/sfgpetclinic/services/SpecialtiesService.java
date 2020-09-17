b0e86f52bdab080d8d563f3b94bd26f8c7eb5d9d
uniloftsky <uniloftsky@gmail.com>
2020-09-18 00:59:54 +0300

    Added specialties service map. Closes #31

---
+++ pet-clinic-data/src/main/java/uniloft/springframework/sfgpetclinic/services/SpecialtiesService.java
@@ -0,0 +1,6 @@
+package uniloft.springframework.sfgpetclinic.services;
+
+import uniloft.springframework.sfgpetclinic.model.Specialty;
+
+public interface SpecialtiesService extends CrudService<Specialty, Long> {
+}
