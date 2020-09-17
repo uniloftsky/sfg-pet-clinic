543fff8f929f57f50d5bc098c83eb9dca58fa7ec
uniloftsky <uniloftsky@gmail.com>
2020-09-18 00:24:45 +0300

    Added PetTypes to bootstrap. Closes #32

--- pet-clinic-web/src/main/java/uniloft/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/uniloft/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -3,8 +3,10 @@ package uniloft.springframework.sfgpetclinic.bootstrap;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import uniloft.springframework.sfgpetclinic.model.Owner;
+import uniloft.springframework.sfgpetclinic.model.PetType;
 import uniloft.springframework.sfgpetclinic.model.Vet;
 import uniloft.springframework.sfgpetclinic.services.OwnerService;
+import uniloft.springframework.sfgpetclinic.services.PetTypeService;
 import uniloft.springframework.sfgpetclinic.services.VetService;
 
 @Component
@@ -12,14 +14,27 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        var dog = new PetType();
+        dog.setName("Dog");
+
+        PetType savedDogPetType =  petTypeService.save(dog);
+
+        var cat = new PetType();
+        cat.setName("Cat");
+
+        PetType savedCatPetType =  petTypeService.save(cat);
+
         var owner1 = new Owner();
         owner1.setFirstName("Michael");
         owner1.setLastName("Wetson");
