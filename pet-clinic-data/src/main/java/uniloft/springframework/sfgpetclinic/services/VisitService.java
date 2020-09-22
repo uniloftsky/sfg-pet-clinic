2c5c4f5cf6e366366e25d922688567a51e51b83a
uniloftsky <uniloftsky@gmail.com>
2020-09-23 00:08:20 +0300

    Created Visit Map Service. Closes #46

---
+++ pet-clinic-data/src/main/java/uniloft/springframework/sfgpetclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package uniloft.springframework.sfgpetclinic.services;
+
+import uniloft.springframework.sfgpetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
--- pet-clinic-data/src/main/java/uniloft/springframework/sfgpetclinic/services/springdatajpa/SpecialtiesSDJpaService.java
+++ pet-clinic-data/src/main/java/uniloft/springframework/sfgpetclinic/services/springdatajpa/SpecialtiesSDJpaService.java
@@ -1,5 +1,7 @@
 package uniloft.springframework.sfgpetclinic.services.springdatajpa;
 
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
 import uniloft.springframework.sfgpetclinic.model.Specialty;
 import uniloft.springframework.sfgpetclinic.repositories.SpecialtyRepository;
 import uniloft.springframework.sfgpetclinic.services.SpecialtyService;
@@ -7,6 +9,8 @@ import uniloft.springframework.sfgpetclinic.services.SpecialtyService;
 import java.util.HashSet;
 import java.util.Set;
 
+@Service
+@Profile("springdatajpa")
 public class SpecialtiesSDJpaService implements SpecialtyService {
 
     private final SpecialtyRepository specialtyRepository;
