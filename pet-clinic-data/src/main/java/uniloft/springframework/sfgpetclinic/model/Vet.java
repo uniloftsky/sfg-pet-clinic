abde0a233325ef76019a6811101856b5b48db5f1
uniloftsky <uniloftsky@gmail.com>
2020-09-22 21:32:39 +0300

    Refactored Vet, Specialty as JPA Entities. Closes #37

--- pet-clinic-data/src/main/java/uniloft/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/uniloft/springframework/sfgpetclinic/model/Vet.java
@@ -1,10 +1,16 @@
 package uniloft.springframework.sfgpetclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "vets")
 public class Vet extends Person {
 
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "vet_specs", joinColumns = @JoinColumn(name = "id_vet"),
+            inverseJoinColumns = @JoinColumn(name = "id_spec"))
     private Set<Specialty> specialties = new HashSet<>();
 
     public Set<Specialty> getSpecialties() {
