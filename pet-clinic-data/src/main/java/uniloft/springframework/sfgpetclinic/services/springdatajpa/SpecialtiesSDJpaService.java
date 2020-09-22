62469930f1c3bc88e67b07d6cc9cf567b36dde88
uniloftsky <uniloftsky@gmail.com>
2020-09-22 22:23:40 +0300

    Created spring data jpa service for specialties. Closes #44

---
+++ pet-clinic-data/src/main/java/uniloft/springframework/sfgpetclinic/services/springdatajpa/SpecialtiesSDJpaService.java
@@ -0,0 +1,44 @@
+package uniloft.springframework.sfgpetclinic.services.springdatajpa;
+
+import uniloft.springframework.sfgpetclinic.model.Specialty;
+import uniloft.springframework.sfgpetclinic.repositories.SpecialtyRepository;
+import uniloft.springframework.sfgpetclinic.services.SpecialtyService;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SpecialtiesSDJpaService implements SpecialtyService {
+
+    private final SpecialtyRepository specialtyRepository;
+
+    public SpecialtiesSDJpaService(SpecialtyRepository specialtyRepository) {
+        this.specialtyRepository = specialtyRepository;
+    }
+
+    @Override
+    public Set<Specialty> findAll() {
+        Set<Specialty> specs = new HashSet<>();
+        specialtyRepository.findAll().forEach(specs::add);
+        return specs;
+    }
+
+    @Override
+    public Specialty findById(Long aLong) {
+        return specialtyRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Specialty save(Specialty object) {
+        return specialtyRepository.save(object);
+    }
+
+    @Override
+    public void delete(Specialty object) {
+        specialtyRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        specialtyRepository.deleteById(aLong);
+    }
+}
