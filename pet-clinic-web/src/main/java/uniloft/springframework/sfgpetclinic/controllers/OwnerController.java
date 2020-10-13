4bdd39c21b5a35eb5f4048f5fd562808c3e9cbbe
unilofsky <uniloftsky@gmail.com>
2020-10-13 13:10:48 +0300

    Created page with owner record displaying

--- pet-clinic-web/src/main/java/uniloft/springframework/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/uniloft/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -2,7 +2,10 @@ package uniloft.springframework.sfgpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 import uniloft.springframework.sfgpetclinic.services.OwnerService;
 
 @RequestMapping("/owners")
@@ -24,6 +27,13 @@ public class OwnerController {
     @RequestMapping("/find")
     public String findOwner() {
         return "notimplemented";
+    }
+
+    @GetMapping("/{id}")
+    public ModelAndView showOwner(@PathVariable String id) {
+        ModelAndView mav = new ModelAndView("owners/ownerDetails");
+        mav.addObject(ownerService.findById(Long.valueOf(id)));
+        return mav;
     }
 
 }
