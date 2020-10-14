e2bee70fbba53ef870ad558f64624a636a9749c3
unilofsky <uniloftsky@gmail.com>
2020-10-14 22:57:51 +0300

    Added ability to create pets. Closes #56

--- pet-clinic-data/src/main/java/uniloft/springframework/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/uniloft/springframework/sfgpetclinic/model/Pet.java
@@ -32,4 +32,8 @@ public class Pet extends BaseEntity {
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
     private Set<Visit> visits = new HashSet<>();
+
+    public Boolean isNew() {
+        return this.getId() == null;
+    }
 }
